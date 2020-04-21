c4a69bbd7835e90e34224cf1cabc43049459176d
Andy Sanchez <andysanchez726@gmail.com>
2020-04-21 14:00:12 -0500

    added function to get user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,12 @@
-var getUserReposs = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserReposs = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos" ;
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
-getUserReposs();
+getUserReposs("andysanchez726");
